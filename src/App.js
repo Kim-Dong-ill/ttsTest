e7479f163fbec472d08700f81776360ec6b3f49d
Kim-Dong-ill <ehddlf2893@naver.com>
2024-09-12 17:16:52 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { useSpeechSynthesis } from 'react-speech-kit';
+import { useSpeechRecognition } from 'react-speech-kit';
 
 function App() {
+  const { speak } = useSpeechSynthesis()
+
+  const [value, setValue] = useState('');
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: (result) => {
+      setValue(result);
+    },
+  });
+  useEffect(() => {
+    if (value.includes("빵빵")) {
+      setTimeout(() => {
+        alert("빵빵 인식 성공!!")
+      }, 2000);
+    }
+  }, [value])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => speak({ text: value })}>음성 test</button>
+      <div>
+        <textarea
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <button onClick={() => { listen({ interimResults: false }) }} >
+          🎤
+        </button>
+        <button onClick={stop}>취소</button>
+        {listening && <div>듣고있어요!</div>}
+        <div>사용자가 한말 : <span>{`" ${value} "`}</span></div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
